521c5f3bd979c4896f43fe36096717c847645a73
lfarrell <lfarrell@email.unc.edu>
2023-08-23 15:52:50 -0400

    Allow user to index all collections if none have ever been indexed.

--- app/jobs/pipette/process_ead_xml_job.rb
+++ app/jobs/pipette/process_ead_xml_job.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require 'pipette/errors'
+require 'pipette'
 
 module Pipette
   class ProcessEadXmlJob < ApplicationJob
@@ -8,11 +9,11 @@ module Pipette
 
     def perform(aspace_id)
       Rails.logger.info "Starting download of EAD for finding aid with ArchivesSpace ID: #{aspace_id}"
-      Pipette::ProcessEad.new.process(aspace_id: aspace_id)
+      ProcessEad.new.process(aspace_id: aspace_id)
       Rails.logger.info "EAD written to file for finding aid with ArchivesSpace ID: #{aspace_id}"
-    rescue Pipette::AspaceRequestError,
-           Pipette::ClassificationError,
-           Pipette::UnpublishedError => e
+    rescue AspaceRequestError,
+           ClassificationError,
+           UnpublishedError => e
       Rails.logger.error("Unable to download and process EAD xml for finding aid with ArchivesSpace ID: #{aspace_id}. Reason: #{e.message}")
     rescue MultiXml::ParseError
       Rails.logger.error("Unable to read xml for finding aid with ArchivesSpace ID: #{aspace_id}. Reason: #{e.backtrace}")
--- lib/pipette.rb
+++ lib/pipette.rb
@@ -1,6 +1,5 @@
 require "pipette/version"
 require "pipette/engine"
-require "pipette"
 
 module Pipette
   # Your code goes here...
