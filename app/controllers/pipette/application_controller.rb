ab50056f797136a09c8f6c66e735935a21243cad
lfarrell <lfarrell@email.unc.edu>
2024-01-12 13:23:26 -0500

    Require that user be a logged in admin to interact with Pipette

--- app/controllers/pipette/application_controller.rb
+++ app/controllers/pipette/application_controller.rb
@@ -1,4 +1,10 @@
 module Pipette
   class ApplicationController < ActionController::Base
+    before_action :authenticate_user!
+    before_action :is_admin?
+
+    def is_admin?
+      redirect_to('/') unless current_user.admin?
+    end
   end
 end
