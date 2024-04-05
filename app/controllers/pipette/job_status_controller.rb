09d97f0295e3da62ca97ac8c620dcfae8140d43f
lfarrell <lfarrell@email.unc.edu>
2024-04-05 11:30:24 -0400

    Don't need any code in the index method

--- app/controllers/pipette/job_status_controller.rb
+++ app/controllers/pipette/job_status_controller.rb
@@ -3,28 +3,7 @@
 # Heavily borrowed from https://github.com/kenaniah/sidekiq-status/blob/v3.0.3/lib/sidekiq-status.rb
 module Pipette
   class JobStatusController < ApplicationController
-    def index
-      jids = redis_adapter do |conn|
-        conn.scan(match: 'sidekiq:status:*', count: 100).map do |key|
-          key.split(':').last
-        end.uniq
-      end
-
-      @job_statuses = []
-
-      jids.each do |jid|
-        status = Sidekiq::Status::get_all jid
-        next if !status || status.count < 2
-
-        status['collection_title'] = field_value(status['collection_title'])
-        status['collection_id'] = field_value(status['collection_id'])
-        status['collecting unit'] = field_value(status['collecting_unit'])
-        status['worker'] = job_type(status['worker'])
-        @job_statuses << status
-      end
-
-      @job_statuses.sort! { |y, x| (x['update_time'] <=> y['update_time']) || 1 }
-    end
+    def index; end
 
     def status_data
       jids = redis_adapter do |conn|
